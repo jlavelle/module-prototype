f304b8ad7bb0520563c790a8c80b9059ef520918
John C. Lavelle <jlavelle604@gmail.com>
2020-10-16 12:29:37 -0400

    Formatting

--- src/defs.test.js
+++ src/defs.test.js
@@ -1,70 +1,74 @@
-import test from 'ava'
-import { functor, applicative, monad } from './defs'
-import { instantiate, module as mod, minimalDef } from './module'
-import { IntSum, Fn } from '@masaeedu/fp'
+import test from "ava";
+import { functor, applicative, monad } from "./defs";
+import { instantiate, module as mod, minimalDef } from "./module";
+import { IntSum, Fn, Arr } from "@masaeedu/fp";
 
 const identityF_ = (() => {
-  const map = f => a => f(a)
-  return { map }
-})()
+  const map = f => a => f(a);
+  return { map };
+})();
 
 const identityA_ = (() => {
-  const of = a => a
-  const ap = fab => fa => fab(fa)
-  return { of, ap }
-})()
+  const of = a => a;
+  const ap = fab => fa => fab(fa);
+  return { of, ap };
+})();
 
 const identityM_ = (() => {
-  const of = a => a
-  const chain = afb => fa => afb(fa)
-  return { of, chain }
-})()
+  const of = a => a;
+  const chain = afb => fa => afb(fa);
+  return { of, chain };
+})();
 
-const identityF = instantiate(functor)(identityF_)
-const identityA = instantiate(applicative)(identityA_)
-const identityM = instantiate(monad)(identityM_)
+const identityF = instantiate(functor)(identityF_);
+const identityA = instantiate(applicative)(identityA_);
+const identityM = instantiate(monad)(identityM_);
 
 test("functor", t => {
-  t.snapshot(identityF)
-  t.is(5, identityF.map(a => a + 1)(4))
-})
+  t.snapshot(identityF);
+  t.is(5, identityF.map(a => a + 1)(4));
+});
 
 test("applicative", t => {
-  t.snapshot(identityA)
-  t.is(5, identityA.map(a => a + 1)(4))
-  t.is(10, identityA.lift2(a => b => a + b)(5)(5))
-  t.is(5, identityA.of(5))
-})
+  t.snapshot(identityA);
+  t.is(5, identityA.map(a => a + 1)(4));
+  t.is(10, identityA.lift2(a => b => a + b)(5)(5));
+  t.is(5, identityA.of(5));
+});
 
 test("monad", t => {
-  t.snapshot(identityM)
-  t.is(5, identityM.map(a => a + 1)(4))
-  t.is(10, identityM.lift2(a => b => a + b)(5)(5))
-  t.is("foo", identityM['*>'](100)('foo'))
-  t.is(10, identityM.of(10))
-  t.is(10, identityM.join(10))
-  t.is(10, identityM.chain(x => x + 5)(5))
-})
+  t.snapshot(identityM);
+  t.is(5, identityM.map(a => a + 1)(4));
+  t.is(10, identityM.lift2(a => b => a + b)(5)(5));
+  t.is("foo", identityM["*>"](100)("foo"));
+  t.is(10, identityM.of(10));
+  t.is(10, identityM.join(10));
+  t.is(10, identityM.chain(x => x + 5)(5));
+});
 
 const arr_ = (() => {
-  const map = f => xs => xs.map(f)
-  const join = xxs => xxs.reduce((acc, x) => acc.concat(x), [])
-  const of = a => [a]
-  const foldl = f => a => xs => xs.reduce((acc, x) => f(acc)(x), a)
+  const map = f => xs => xs.map(f);
+  const join = xxs => xxs.reduce((acc, x) => acc.concat(x), []);
+  const of = a => [a];
+  const foldl = f => a => xs => xs.reduce((acc, x) => f(acc)(x), a);
 
   return mod.defModule({
     name: "Arr",
     mdef: minimalDef.empty,
     methods: () => ({
-      map, join, of, foldl
-    }),
-  })
-})()
+      map,
+      join,
+      of,
+      foldl
+    })
+  });
+})();
 
-const arr = instantiate(mod.append(arr_)(monad))({})
+//const arr = instantiate(mod.append(arr_)(monad))({})
+const arr = instantiate(Arr.fold(mod)([arr_, functor, applicative, monad]))({});
 
 test("arr", t => {
-  t.snapshot(arr)
-  t.deepEqual([1,1,2,2,3,3], arr.lift2(Fn.const)([1,2,3])([1,2]))
-  t.deepEqual([2,3,3,4,4,5], arr.chain(a => [a + 1, a + 2])([1,2,3]))
-})
+  t.snapshot(arr);
+  t.deepEqual([1, 1, 2, 2, 3, 3], arr.lift2(Fn.const)([1, 2, 3])([1, 2]));
+  t.deepEqual([2, 3, 3, 4, 4, 5], arr.chain(a => [a + 1, a + 2])([1, 2, 3]));
+});
